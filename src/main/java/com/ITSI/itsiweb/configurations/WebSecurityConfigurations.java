d7e159de33c1301334ab65ffedc4664568224fa1
JpRod96 <jprodriguez60@gmail.com>
2020-04-24 08:55:37 -0400

    Add first configuration of http security with spring security

---
+++ src/main/java/com/ITSI/itsiweb/configurations/WebSecurityConfigurations.java
@@ -0,0 +1,30 @@
+package com.ITSI.itsiweb.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable()
+                //.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/user").permitAll()
+                .anyRequest().authenticated();
+    }
+}
--- src/main/java/com/ITSI/itsiweb/entities/users/User.java
+++ src/main/java/com/ITSI/itsiweb/entities/users/User.java
@@ -39,4 +39,12 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
