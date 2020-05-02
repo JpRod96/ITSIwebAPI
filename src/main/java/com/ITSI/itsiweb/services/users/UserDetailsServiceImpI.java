d141dcfb5d55cedf0d343a973b0eceaa2e1af3a8
JpRod96 <jprodriguez60@gmail.com>
2020-05-02 18:22:57 -0400

    Add UserDetailsService for injection

---
+++ src/main/java/com/ITSI/itsiweb/services/users/UserDetailsServiceImpI.java
@@ -0,0 +1,28 @@
+package com.ITSI.itsiweb.services.users;
+
+import com.ITSI.itsiweb.repositories.users.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static java.util.Collections.emptyList;
+
+@Service
+public class UserDetailsServiceImpI implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        com.ITSI.itsiweb.entities.users.User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new User(user.getUsername(), user.getPassword(), emptyList());
+    }
+}
