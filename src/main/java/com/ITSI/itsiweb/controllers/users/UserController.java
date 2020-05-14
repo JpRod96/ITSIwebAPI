53574ff77be7c58524eccf307c341949ceffb860
JpRod96 <jprodriguez60@gmail.com>
2020-05-14 19:39:13 -0400

    Getting info of logged user when getting /user

--- src/main/java/com/ITSI/itsiweb/controllers/users/UserController.java
+++ src/main/java/com/ITSI/itsiweb/controllers/users/UserController.java
@@ -4,6 +4,8 @@ import com.ITSI.itsiweb.entities.users.User;
 import com.ITSI.itsiweb.services.users.User.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,21 +16,23 @@ import static com.ITSI.itsiweb.utils.RoleAuthorizationConstants.*;
 public class UserController {
     @Autowired
     private UserService userService;
+    private final String USER_ROOT = "/user";
 
     @PreAuthorize(ADMINISTRATOR)
-    @PostMapping("/user")
+    @PostMapping(USER_ROOT)
     public void saveUser(@RequestBody User user) {
         userService.save(user);
     }
 
-    @PreAuthorize(ADMINISTRATOR + OR + STUDENT)
-    @GetMapping("/user/{username}")
-    public User getUserByUsername(@PathVariable("username")String username){
-        return userService.findBy(username);
+    @PreAuthorize(ADMINISTRATOR + OR + STUDENT + OR + ACCOUNTANT + OR + AUXILIARY_ACCOUNTANT)
+    @GetMapping(USER_ROOT)
+    public User getLoggedUser(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return userService.findBy(auth.getName());
     }
 
-    @PreAuthorize(STUDENT)
-    @GetMapping("/users")
+    @PreAuthorize(ADMINISTRATOR)
+    @GetMapping(USER_ROOT + "/all")
     public List<User> getAllUsers(){
         return userService.listAllUsers();
     }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,6 @@
 INSERT INTO role (id, name) VALUES (1, 'ADMINISTRATOR');
 INSERT INTO role (id, name) VALUES (2, 'STUDENT');
+INSERT INTO role (id, name) VALUES (3, 'ACCOUNTANT');
 INSERT INTO user (id, username, password, role_id) VALUES (1, 'admin', '$2a$10$XURPShQNCsLjp1ESc2laoObo9QZDhxz73hJPaEv7/cBha4pk0AgP.', 1);
 INSERT INTO user (id, username, password, role_id) VALUES (2, 'est', '$2a$10$XURPShQNCsLjp1ESc2laoObo9QZDhxz73hJPaEv7/cBha4pk0AgP.', 2);
+INSERT INTO user (id, username, password, role_id) VALUES (3, 'acc', '$2a$10$XURPShQNCsLjp1ESc2laoObo9QZDhxz73hJPaEv7/cBha4pk0AgP.', 3);
